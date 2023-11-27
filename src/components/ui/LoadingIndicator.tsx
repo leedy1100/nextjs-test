7fbf1b2f2a49581e53df03d6640a2a174078bf1f
이두영 <dlendud@hubilon.com>
2023-11-27 16:41:14 +0900

    Fix: swiper 스켈레톤 ui 수정

--- src/components/ui/LoadingIndicator.tsx
+++ src/components/ui/LoadingIndicator.tsx
@@ -5,19 +5,19 @@ export default function LoadingIndicator() {
   return (
     <ContentLoader
       speed={2}
-      viewBox="0 0 630 235"
+      viewBox="0 0 620 235"
       backgroundColor="#f0f0f0"
       foregroundColor="#dedede"
     >
       <rect x="0" y="0" rx="5" ry="5" width="200" height="200" />
       <rect x="210" y="0" rx="5" ry="5" width="200" height="200" />
       <rect x="420" y="0" rx="5" ry="5" width="200" height="200" />
-      <rect x="0" y="210" rx="0" ry="0" width="200" height="10" />
-      <rect x="210" y="210" rx="0" ry="0" width="200" height="10" />
-      <rect x="420" y="210" rx="0" ry="0" width="200" height="10" />
-      <rect x="0" y="225" rx="0" ry="0" width="200" height="10" />
-      <rect x="210" y="225" rx="0" ry="0" width="200" height="10" />
-      <rect x="420" y="225" rx="0" ry="0" width="200" height="10" />
+      <rect x="0" y="210" rx="3" ry="3" width="200" height="10" />
+      <rect x="210" y="210" rx="3" ry="3" width="200" height="10" />
+      <rect x="420" y="210" rx="3" ry="3" width="200" height="10" />
+      <rect x="0" y="225" rx="3" ry="3" width="200" height="10" />
+      <rect x="210" y="225" rx="3" ry="3" width="200" height="10" />
+      <rect x="420" y="225" rx="3" ry="3" width="200" height="10" />
     </ContentLoader>
   );
 }
