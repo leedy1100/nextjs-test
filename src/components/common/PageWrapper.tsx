7b2d3d4245bfc1c9ae0d57627d5dc97771d79ff5
이두영 <dlendud@hubilon.com>
2024-01-26 16:59:03 +0900

    Fix: 화면 전환 효과 변경
    y축으로 올라가는 애니메이션 효과 제거.

--- src/components/common/PageWrapper.tsx
+++ src/components/common/PageWrapper.tsx
@@ -11,9 +11,9 @@ export default function PageWrapper({ children }: Props) {
   return (
     <AnimatePresence>
       <motion.div
-        initial={{ opacity: 0, y: 15 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 15 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
         transition={{ duration: 0.25 }}
       >
         {children}
