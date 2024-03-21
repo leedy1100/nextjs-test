194aadd9e20349b280f5bcd6ca1bcc543d75c549
이두영 <dlendud@hubilon.com>
2024-03-21 14:48:09 +0900

    Fix: sns 로그인 버튼 스타일 변경

--- src/components/signin/Signin.tsx
+++ src/components/signin/Signin.tsx
@@ -10,18 +10,35 @@ type Props = {
 
 export default function Signin({ providers, callbackUrl }: Props) {
   return (
-    <div className="">
-      <div className="flex flex-col justify-center items-center gap-4">
+    <div className="w-full">
+      <div className="flex flex-col m-4 gap-4">
         {Object.values(providers).map(
           ({ name, id }) =>
             id !== 'credentials' && (
               <button key={id} onClick={() => signIn(id, { callbackUrl })}>
-                <Image
-                  src={`/assets/images/login/${name}.png`}
-                  alt={name}
-                  width={200}
-                  height={100}
-                />
+                {name === 'Naver' && (
+                  <div className="flex justify-center items-center bg-[#03C75A] rounded-full min-h-[50px]">
+                    <Image
+                      src="/assets/images/login/naverLogo.png"
+                      alt="naver"
+                      width={50}
+                      height={50}
+                    />
+                    <p className="text-white">네이버로 계속하기</p>
+                  </div>
+                )}
+                {name === 'Kakao' && (
+                  <div className="flex justify-center items-center bg-[#FEE500] rounded-full min-h-[50px]">
+                    <Image
+                      src="/assets/images/login/kakaoLogo.svg"
+                      alt="kakao"
+                      width={25}
+                      height={25}
+                      className="m-3"
+                    />
+                    <p className="text-black">카카오로 계속하기</p>
+                  </div>
+                )}
               </button>
             ),
         )}
