867e42cbdf09418151d8222b93a6e7272f3edb70
leeduyeong <dy921119@gmail.com>
2024-09-26 10:23:48 +0900

    Fix: etc 수정

--- src/app/etc/page.tsx
+++ src/app/etc/page.tsx
@@ -9,7 +9,7 @@ export default function page() {
   return (
     <PageWrapper>
       <div>
-        <h1>Etc!!!</h1>
+        <h1>Etc!!!!</h1>
         <div className="flex flex-wrap justify-start items-center gap-4 m-4">
           <AnimatedButton animationType="bounce" text="bounce" />
           <AnimatedButton animationType="float" text="float" />
@@ -21,33 +21,15 @@ export default function page() {
           <PresenceComponent />
           <div>
             <h1 className="mb-2">Pretendard Font Weight!</h1>
-            <p className="font-thin">
-              Pretendard font-thin 안녕하세요. 프리텐다드 무료 폰트입니다.
-            </p>
-            <p className="font-extralight">
-              Pretendard font-extralight 안녕하세요. 프리텐다드 무료 폰트입니다.
-            </p>
-            <p className="font-light">
-              Pretendard font-light 안녕하세요. 프리텐다드 무료 폰트입니다.
-            </p>
-            <p className="font-normal">
-              Pretendard font-normal 안녕하세요. 프리텐다드 무료 폰트입니다.
-            </p>
-            <p className="font-medium">
-              Pretendard font-medium 안녕하세요. 프리텐다드 무료 폰트입니다.
-            </p>
-            <p className="font-semibold">
-              Pretendard font-semibold 안녕하세요. 프리텐다드 무료 폰트입니다.
-            </p>
-            <p className="font-bold">
-              Pretendard font-bold 안녕하세요. 프리텐다드 무료 폰트입니다.
-            </p>
-            <p className="font-extrabold">
-              Pretendard font-extrabold 안녕하세요. 프리텐다드 무료 폰트입니다.
-            </p>
-            <p className="font-black">
-              Pretendard font-black 안녕하세요. 프리텐다드 무료 폰트입니다.
-            </p>
+            <p className="font-thin">Pretendard font-thin 안녕하세요. 프리텐다드 무료 폰트입니다.</p>
+            <p className="font-extralight">Pretendard font-extralight 안녕하세요. 프리텐다드 무료 폰트입니다.</p>
+            <p className="font-light">Pretendard font-light 안녕하세요. 프리텐다드 무료 폰트입니다.</p>
+            <p className="font-normal">Pretendard font-normal 안녕하세요. 프리텐다드 무료 폰트입니다.</p>
+            <p className="font-medium">Pretendard font-medium 안녕하세요. 프리텐다드 무료 폰트입니다.</p>
+            <p className="font-semibold">Pretendard font-semibold 안녕하세요. 프리텐다드 무료 폰트입니다.</p>
+            <p className="font-bold">Pretendard font-bold 안녕하세요. 프리텐다드 무료 폰트입니다.</p>
+            <p className="font-extrabold">Pretendard font-extrabold 안녕하세요. 프리텐다드 무료 폰트입니다.</p>
+            <p className="font-black">Pretendard font-black 안녕하세요. 프리텐다드 무료 폰트입니다.</p>
           </div>
         </div>
         <OptimizationTable />
